6067a4663ac3671dede09ded4608f95feef56ffe
Georges Dahdouh <georges.dahdouh@gmail.com>
2022-03-03 11:29:05 +0200

    correct a comment

--- Select_Corner_Edges.py
+++ Select_Corner_Edges.py
@@ -1,6 +1,6 @@
 # Script by Georges Dahdouh
 # Licence GPL 3.0
-# This script assumes being used in Edit Mode, mesh object, at least 1 corner edge selected in and edge loop.
+# This script assumes being used in Edit Mode, mesh object, at least 1 corner edge selected in and edge ring.
 # Run this script in the script editor, then use F3 and search for select corner edges operator.
 # Happy blending!
 
